6017ec591d57a0c55e4bd2a68293d424194cbda1
Derek Chan <derekchancts@yahoo.com.hk>
2020-09-17 17:30:05 +0800

    Updates

---
+++ precache-manifest.06b98be624f4c3be274b32d53b89bf57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc62429bec6b8e7ccc9f22c9d0350e7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9effba92012e62d4f30c",
+    "url": "/monsters-rolodex/static/css/main.4116fe03.chunk.css"
+  },
+  {
+    "revision": "c82828649db0b71a8d1f",
+    "url": "/monsters-rolodex/static/js/2.7deb2424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7deb2424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9effba92012e62d4f30c",
+    "url": "/monsters-rolodex/static/js/main.fbefac57.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
